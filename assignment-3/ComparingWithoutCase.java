bc482d1baae61e2430cac7e24303630f92aebc92
yasaswini1910 <yasaswinigorle@gmail.com>
2026-09-10 20:56:18 +0530

    Add ComparingWithoutCase class for case-insensitive comparison

---
+++ assignment-3/ComparingWithoutCase.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class ComparingWithoutCase {
+public static void main(String[] args) {
+	String s1 = "JAVA";
+	String s2 = "java";
+	System.out.println(s1.equalsIgnoreCase(s2));
+}
+}
