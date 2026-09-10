ef3af4e385f6e47a048c92ec54d1115a0a6fb37d
yasaswini1910 <yasaswinigorle@gmail.com>
2026-09-10 20:17:40 +0530

    Add Extracting class to demonstrate string manipulation

---
+++ assignment-3/Extracting.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class Extracting {
+public static void main(String[] args) {
+	String s = "Extrodinary";
+	System.out.println(s.substring(2));
+	System.out.println(s.substring(4,7));
+}
+}
