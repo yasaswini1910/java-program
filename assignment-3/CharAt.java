6d35f78262d883f4e466cf2bdcc9df6b2a72dec6
yasaswini1910 <yasaswinigorle@gmail.com>
2026-09-10 22:27:27 +0530

    Add CharAt class to demonstrate charAt usage

---
+++ assignment-3/CharAt.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class CharAt {
+public static void main(String[] args) {
+	String s = "Extrodinary";
+	System.out.println(s.charAt(0));
+	System.out.println(s.charAt(6));
+}
+}
