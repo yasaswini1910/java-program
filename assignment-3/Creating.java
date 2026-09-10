4ccb5725a3408db24f0d8c875d98d31011a14e04
yasaswini1910 <yasaswinigorle@gmail.com>
2026-09-10 20:26:59 +0530

    Create Creating.java

---
+++ assignment-3/Creating.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class Creating {
+	public static void main(String[] args) {
+		String s1 = "Random";
+		String s2 = "Super";
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
